e90560512fd97fd64f1c181018398aeffc9ce5ed
ranji <ranjithstar256@gmail.com>
2021-10-21 00:43:03 +0530

    added CRUD

--- app/src/main/java/ran/am/mysqlitedb/DBHlpr.kt
+++ app/src/main/java/ran/am/mysqlitedb/DBHlpr.kt
@@ -5,35 +5,42 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.location.Location
+import java.util.jar.Attributes
 
-class DBHlpr(context: Context) : SQLiteOpenHelper(context, "details.db", null, 1){
+class DBHlpr(context: Context) : SQLiteOpenHelper(context, "details.db", null, 1) {
 
-    var sqLiteDatabase : SQLiteDatabase = writableDatabase
+    var sqLiteDatabase: SQLiteDatabase= writableDatabase
+
 
     override fun onCreate(db: SQLiteDatabase?) {
+
         if (db != null) {
             db.execSQL("create table students (Name text , Location text)")
         }
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 
     fun savedat(s1: String, s2: String): Long {
+        sqLiteDatabase=writableDatabase
 
         val cv = ContentValues()
-        cv.put("Name",s1)
-        cv.put("Location",s2)
-        return sqLiteDatabase.insert("students",null,cv)
+        cv.put("Name", s1)
+        cv.put("Location", s2)
+        return sqLiteDatabase.insert( "students", null, cv)
 
     }
 
     fun retrive(s1: String): String {
-      //  sqLiteDatabase=writableDatabase
+        //  sqLiteDatabase=writableDatabase
 
-        var c : Cursor = sqLiteDatabase.query("students",null,"Name=?", arrayOf(s1),null
-        ,null,null)
+        var c: Cursor = sqLiteDatabase.query(
+            "students", null, "Name=?", arrayOf(s1), null, null, null
+        )
 
-        if (c.count<1){
+        if (c.count < 1) {
             return "name not exists"
         }
 
@@ -45,14 +52,17 @@ class DBHlpr(context: Context) : SQLiteOpenHelper(context, "details.db", null, 1
     }
 
     fun updateloc(s1: String, s2: String): Int {
-        //sqLiteDatabase=writableDatabase
+        sqLiteDatabase=writableDatabase
         val cv = ContentValues()
-        cv.put("Location",s2)
-       var j = sqLiteDatabase.update("students",cv,"Name = $s1", null)
+        cv.put("Location", s2)
+        var j = sqLiteDatabase.update("students", cv, "Name = ?", arrayOf(s1))
         return j
     }
 
     fun delname(s1: String) {
-        sqLiteDatabase.delete("students","Name = $s1",null)
+        sqLiteDatabase=writableDatabase
+
+        sqLiteDatabase.delete("students", "Name =?",  arrayOf(s1))
     }
+ 
 }
--- app/src/main/java/ran/am/mysqlitedb/MainActivity.kt
+++ app/src/main/java/ran/am/mysqlitedb/MainActivity.kt
@@ -2,6 +2,7 @@ package ran.am.mysqlitedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,20 +10,27 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var edname : EditText
+    lateinit var edname2 : EditText
+    lateinit var edname3 : EditText
+    lateinit var edname4 : EditText
     lateinit var edloc : EditText
+    lateinit var edloc2 : EditText
     lateinit var tv : TextView
     lateinit var btnsave : Button
     lateinit var btnretrive : Button
     lateinit var btnupdate : Button
     lateinit var btndel : Button
-    var s1 = ""
-    var s2 = ""
+    
     lateinit var dbhr : DBHlpr
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         edname=findViewById(R.id.editTextTextPersonName)
+        edname2=findViewById(R.id.editTextTextPersonName4)
+        edname3=findViewById(R.id.editTextTextPersonName5)
+        edname4=findViewById(R.id.editTextTextPersonName7)
         edloc=findViewById(R.id.editTextTextPersonName2)
+        edloc2=findViewById(R.id.editTextTextPersonName6)
         tv=findViewById(R.id.textView)
         btnsave=findViewById(R.id.button)
         btnretrive=findViewById(R.id.button2)
@@ -32,31 +40,33 @@ class MainActivity : AppCompatActivity() {
         dbhr = DBHlpr(applicationContext)
 
         btnsave.setOnClickListener {
-            s1=edname.text.toString()
-            s2=edloc.text.toString()
+            var s1=edname.text.toString()
+            var s2=edloc.text.toString()
            var l = dbhr.savedat(s1,s2)
             Toast.makeText(applicationContext, "data saved successfully! "+l, Toast.LENGTH_SHORT).show();
         }
 
         btnretrive.setOnClickListener {
-            s1=edname.text.toString()
-            var loc = dbhr.retrive(s1)
+            var loc = dbhr.retrive(edname2.text.toString())
+
             tv.text= loc
+            Toast.makeText(applicationContext, "retrive success"+loc, Toast.LENGTH_SHORT).show();
+            
         }
 
         btnupdate.setOnClickListener {
-            s1=edname.text.toString()
-            s2=edloc.text.toString()
+           // var s1=
+           // var s2=
 
-           var u= dbhr.updateloc(s1,s2)
-            Toast.makeText(applicationContext, ""+u, Toast.LENGTH_SHORT).show();
+           var u= dbhr.updateloc(edname3.text.toString(),edloc2.text.toString())
+            Toast.makeText(applicationContext, "update success"+u, Toast.LENGTH_SHORT).show();
 
         }
 
         btndel.setOnClickListener {
-            s1=edname.text.toString()
+            var s1=edname4.text.toString()
             dbhr.delname(s1)
-
+            Toast.makeText(applicationContext, "delete success!", Toast.LENGTH_SHORT).show();
 
         }
     }
